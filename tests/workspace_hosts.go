3ed3bc5b4a0296f805dc34f96dad30f31ade9b63
Wolfy-J <wolfy.jd@gmail.com>
2026-09-25 11:07:46 -0400

    test: overlay delayed broker at current source path

--- tests/workspace_hosts.go
+++ tests/workspace_hosts.go
@@ -149,7 +149,7 @@ func run() error {
 		if err != nil {
 			return err
 		}
-		if err := os.WriteFile(filepath.Join(root, "src", "core", "applications", "broker.lua"), broker, 0600); err != nil {
+		if err := os.WriteFile(filepath.Join(root, "src", "applications", "broker.lua"), broker, 0600); err != nil {
 			return err
 		}
 	}
